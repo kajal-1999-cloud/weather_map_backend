7669f8e3112ddf80ac3c2ebe368280eba6aeccdb
kajal Gupta <kajalg1401@gmail.com>
2025-12-12 00:32:25 +0530

    new data

--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -1,6 +1,21 @@
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+// import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+// import markerIcon from "leaflet/dist/images/marker-icon.png";
+// import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: markerIcon2x,
+//   iconUrl: markerIcon,
+//   shadowUrl: markerShadow,
+// });
+
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
 import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
